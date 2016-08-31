d365e4c2af4035341ec7e0ab1d182dee71ed7165
Jonathan Colon <jonathancolon@C4Qs-MacBook-Pro-85.local>
2016-08-31 19:00:25 -0400

    Hangman Prework Submission

--- src/nyc/c4q/Main.java
+++ src/nyc/c4q/Main.java
@@ -1,24 +1,68 @@
 package nyc.c4q;
 
+import java.util.Scanner;
+
 public class Main {
 
+    static Scanner prompt = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Hangman hangman = new Hangman();
-        while (hangman.getMisses() < 5){
-            hangman.printCurrentWord();
-            hangman.promptPlayer("Enter a letter: ");
-            hangman.readLetter();
-            hangman.checkLetter();
-            if(hangman.guessedSuccessfully()){
-                break;
+
+
+        boolean notDone = true;
+
+        do {
+            Hangman hangman = new Hangman();
+
+
+            while (hangman.getMisses() < 5) {
+                hangman.printCurrentWord();
+                hangman.promptPlayer("Enter a letter: ");
+                hangman.readLetter();
+                hangman.checkLetter();
+                if (hangman.guessedSuccessfully()) {
+                    break;
+                }
+                System.out.println(Drawing.get(hangman.getMisses()));
+                System.out.println("Misses -> " + hangman.getMisses());
             }
-            System.out.println(Drawing.get(hangman.getMisses()));
-            System.out.println("Misses -> " +hangman.getMisses());
-        }
-        if(hangman.guessedSuccessfully()){
-            System.out.println("Success");
-        } else {
-            System.out.println("The answer was " + hangman.getSecretWord());
-        }
+            if (hangman.guessedSuccessfully()) {
+                System.out.println("Success");
+            } else {
+                System.out.println("The answer was " + hangman.getSecretWord());
+            }
+
+
+
+            boolean isValidInput = false;
+
+
+
+            do {
+                System.out.println("Do you want to play again?");
+                String getInput = String.valueOf(prompt.next());
+                String userAnswer = getInput;
+                userAnswer = userAnswer.toLowerCase();
+
+                if (userAnswer.equals("yes") || userAnswer.equals("y") ) {
+
+                    notDone = true;
+                    isValidInput = true;
+
+
+
+                } else if (userAnswer.equals("no") || userAnswer.equals("n") ) {
+
+                    notDone = false;
+                    isValidInput = true;
+
+                } else {
+                    System.out.println("Try Again!");
+                }
+            } while (isValidInput == false);
+
+
+        } while (notDone);
     }
+
 }
